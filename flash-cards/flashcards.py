bcb314f402e1580eb47fbbf0e97398e63f080634
sharmaineb <sharmaine.borbe@students.dominican.edu>
2022-09-06 13:58:53 -0700

    completed 'bear, ninja, cowboy, game'

--- flash-cards/flashcards.py
+++ flash-cards/flashcards.py
@@ -1,12 +1,18 @@
+# flashcards.py
+# imported the json module from python3
 import json
 
+
+# opened the file and parse from python3
 with open('me-capitals.json', 'r') as f:
     data = json.load(f)
 
+    
     total = len(data["cards"])
 
-    score = 0
-        
+score = 0
+
+
 for i in data["cards"]:
     guess = input(i["q"] + " > ")
     
@@ -17,5 +23,18 @@ for i in data["cards"]:
         print("Incorrect! The correct answer was", i["a"])
         print(f"Current Score: {score}/{total}")
 
+    play_again = input(f"Ready for the next card? (yes/no): ")
+    if play_again == "yes":
+        player = False
+    else:
+      break
+
+    
+
+
+
+
+
+    
 
 
